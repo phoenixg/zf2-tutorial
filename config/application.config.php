d4cf2c56f4c0faf5bc5f253784ff8db726975af6
phoenixg-compaq321 <306761352@qq.com>
2013-07-06 15:00:37 +0800

    add ZFTool

--- config/application.config.php
+++ config/application.config.php
@@ -4,6 +4,7 @@ return array(
     'modules' => array(
         'Tutorial',
         'Album',
+        'ZFTool',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
