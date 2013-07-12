70605dd9b6bcb57f39281887157d2a45fdb80651
Huang Feng <306761352@qq.com>
2013-07-12 12:19:41 +0800

    config

--- config/application.config.php
+++ config/application.config.php
@@ -39,23 +39,23 @@ return array(
         'config_cache_enabled' => ($env == 'production'),
 
         // The key used to create the configuration cache file name.
-        'config_cache_key' => 'app_config',
+        // 'config_cache_key' => 'app_config',
 
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
-        'module_map_cache_enabled' => ($env == 'production'),
+        // 'module_map_cache_enabled' => ($env == 'production'),
 
         // The key used to create the class map cache file name.
-        'module_map_cache_key' => 'module_map',
+        // 'module_map_cache_key' => 'module_map',
 
         // The path in which to cache merged configuration.
-        'cache_dir' => 'data/config/',
+        // 'cache_dir' => 'data/config/',
 
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
         // that weren't loaded.
-         'check_dependencies' => ($env != 'production'),
+        //  'check_dependencies' => ($env != 'production'),
     ),
 
     // Used to create an own service manager. May contain one or more child arrays.
